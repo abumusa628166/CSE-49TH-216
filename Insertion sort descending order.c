07d18d24aebb207a7a0e397969340d358a93631a
MD.ABU MUSA <59367232+abumusa628166@users.noreply.github.com>
2020-01-02 22:59:37 +0600

    Update Insertion sort descending order.c

--- Insertion sort descending order.c
+++ Insertion sort descending order.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-int insertionSortAsc(int A[],int n);
+int insertionSortDsc(int A[],int n);
 void display(int A[],int n);
 int main(){
     int a[]={69,35,84,72,68,51,22,34,71,55};
     int i,n=10;
     printf("Unsorted Array\n");
-    for(i=0;i<10;i++){
+    for(i=0;i<n;i++){
         printf("%d\t",a[i]);
     }
     printf("\n");
-    printf("Insertion Sort In Ascending Order\n");
-    insertionSortAsc(a,n);
+    printf("Insertion Sort In Descending Order\n");
+    insertionSortDsc(a,n);
     return 0;
 }
 
-int insertionSortAsc(int A[],int n){
+int insertionSortDsc(int A[],int n){
     int i,j,B[n];
 
     for(i=0; i<n; i++){
